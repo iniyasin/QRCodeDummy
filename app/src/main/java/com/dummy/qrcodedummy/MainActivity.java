c857769ee45fbcd19457629b23b97b238b31797f
Muchammad Yaasiin <iniyasin7@gmail.com>
2019-07-04 08:49:56 +0700

    Initial commit

---
+++ app/src/main/java/com/dummy/qrcodedummy/MainActivity.java
@@ -0,0 +1,30 @@
+package com.dummy.qrcodedummy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public static TextView tv_result;
+    public Button btn_qr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_result = (TextView) findViewById(R.id.tv_result);
+        btn_qr = (Button) findViewById(R.id.btn_qr);
+
+        btn_qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/dummy/qrcodedummy/ScanActivity.java
@@ -0,0 +1,39 @@
+package com.dummy.qrcodedummy;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    public ZXingScannerView mScannerView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        MainActivity.tv_result.setText(rawResult.getText());
+        onBackPressed();
+    }
+}
